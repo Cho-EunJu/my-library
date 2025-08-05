3278163ac5a4bf257286f37ee8f3fa76a074e076
name <ej.dev.929@gmail.com>
2025-08-05 21:16:13 +0900

    로그인 컴포넌트 퍼블리싱 완료

--- my-library-front/src/main.ts
+++ my-library-front/src/main.ts
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'      // ★ 추가
 // import store from './store'
+import '@/assets/css/base.css'
 
 // createApp(App).use(router).use(store).mount('#app')
 createApp(App).use(createPinia()).use(router).mount('#app')
--- my-library-front/src/pages/Login.vue
+++ my-library-front/src/pages/Login.vue
@@ -2,8 +2,44 @@
 
 </script>
 
+<style scoped src="@/assets/css/login.css"></style>
 <template>
-  <h1>login</h1>
+    <main class="form-signin text-center">
+      <form>
+        <h1 class="site-title">
+          <i class="bi bi-book-half"></i>
+          모두의 <span class="highlight">도서관</span>
+        </h1>
+
+        <div class="form-floating">
+          <input type="text" class="form-control" id="floatingInput" placeholder="ID">
+          <label for="floatingInput">아이디</label>
+        </div>
+        <div class="form-floating">
+          <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+          <label for="floatingPassword">비밀번호</label>
+        </div>
+
+        <button class="w-100 btn btn-lg btn-primary mt-3 mb-4" type="submit">로그인</button>
+
+        <ul class="nav nav-pills nav-gray-list">
+          <li class="nav-gray-item"><router-link to="/" class="nav-link" active-class="active" aria-current="page">비밀번호 찾기</router-link></li>
+          <li class="nav-gray-item"><router-link to="/wish" class="nav-link" active-class="active">회원가입</router-link></li>
+          <li class="nav-gray-item"><router-link to="/hist" class="nav-link" active-class="active">아이디찾기</router-link></li>
+        </ul>
+
+        <div class="social-login-wrapper">
+          <div class="social-login-title">
+            <span>간편 로그인</span>
+          </div>
+          <div class="social-login-buttons">
+            <button class="social-btn kakao" aria-label="카카오 로그인" title="카카오"></button>
+            <button class="social-btn naver" aria-label="네이버 로그인" title="네이버"></button>
+            <button class="social-btn google" aria-label="구글 로그인" title="구글"></button>
+          </div>
+        </div>
+      </form>
+    </main>
 </template>
 
 <style scoped>
